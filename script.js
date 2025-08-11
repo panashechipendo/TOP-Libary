069162670121225745b6b7fc8cc767d92690af12
Panashe Chipendo <106480889+panashechipendo@users.noreply.github.com>
2025-08-11 16:56:38 +0200

    Start coding logic for adding books

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const newBookBtn = document.querySelector(".new-book");
 const sideBar = document.querySelector(".sidebar");
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 
 function hideSidebar() {
   sideBar.classList.toggle("hidden");
 }
 
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  // the constructor...
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  // take params, create a book then store it in the array
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
 newBookBtn.addEventListener("click", hideSidebar);
-form.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    console.log(e)
-})
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
