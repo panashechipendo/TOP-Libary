a2d48623e3de56c94f23c667c7a79a3e2e230609
Panashe Chipendo <106480889+panashechipendo@users.noreply.github.com>
2025-08-11 20:16:30 +0200

    Fix bug where books added were textless

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ const sideBar = document.querySelector(".sidebar");
 const form = document.querySelector("form");
 const display = document.querySelector(".main-content");
 
-let innerEl = [
-  document.createElement("h3"),
-  document.createElement("p"),
-  document.createElement("p"),
-  document.createElement("p"),
-];
 const title1 = document.createElement("h3");
 const author1 = document.createElement("p");
 const pages1 = document.createElement("p");
@@ -53,6 +47,13 @@ function addToDisplay(arr) {
   // take array, create element then display it on DOM
   for (item of arr) {
     const book1 = document.createElement("div");
+    let innerEl = [
+      document.createElement("h3"),
+      document.createElement("p"),
+      document.createElement("p"),
+      document.createElement("p"),
+      document.createElement("p"),
+    ];
 
     const innerEl2 = Array.from(innerEl);
 
@@ -60,13 +61,20 @@ function addToDisplay(arr) {
     innerEl2[1].textContent = `Author: ${item.author}`;
     innerEl2[2].textContent = `Pages: ${item.pages}`;
     innerEl2[3].textContent = item.read == true ? "Read?: yes" : "Read?: no";
+    innerEl2[4].textContent = `Id: ${item.id}`;
 
     book1.classList.toggle("book");
     for (entry of innerEl) {
+      console.log(entry);
       book1.append(entry);
     }
     display.append(book1);
   }
+}
+
+function clearDisplay() {
+  const books = document.querySelectorAll(".book");
+  books.forEach((div) => div.remove());
 }
 
 newBookBtn.addEventListener("click", hideSidebar);
@@ -80,8 +88,10 @@ form.addEventListener("submit", (event) => {
   const selectedEl = document.querySelector("input[name='read-state']:checked");
   const selectValue = selectedEl.value === "true";
 
+  clearDisplay();
+
   addBookToLibrary(title, author, pages, selectValue);
   addToDisplay(myLibrary);
   console.log(myLibrary);
-  console.log(myLibrary[0].title);
+  console.log();
 });
